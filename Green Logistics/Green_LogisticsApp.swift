6b31a7da57250c90baa4e6cad8d12c2fd61c4cdf
Meech The Innovator <mindofmeech@gmail.com>
2024-08-21 03:08:19 -0500

    Initial Commit

---
+++ Green Logistics/Green_LogisticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Green_LogisticsApp.swift
+//  Green Logistics
+//
+//  Created by Demetrius Norman on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Green_LogisticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Green LogisticsUITests/Green_LogisticsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Green_LogisticsUITestsLaunchTests.swift
+//  Green LogisticsUITests
+//
+//  Created by Demetrius Norman on 8/21/24.
+//
+
+import XCTest
+
+final class Green_LogisticsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
